f2fc392d4d35794ec27f6f5d0e739f50d0e98f81
arnoldout <arnoldout@gmail.com>
2017-04-18 19:48:04 +0100

    Removed dud code

--- src/ie/gmit/sw/ai/sprites/BlackSpider.java
+++ src/ie/gmit/sw/ai/sprites/BlackSpider.java
@@ -12,51 +12,10 @@ public class BlackSpider extends Spider {
 	}
 	@Override
 	public void run() {
-		try{
-			DistanceRisk pickupRisk = SpiderService.getInstance().getPickupRisk(getModel(), getX(), getY(), getHealth());
-			DistanceRisk spartanRisk = SpiderService.getInstance().getSpartanRisk(getX(), getY(), getHealth());
-			DistanceRisk friendlyRisk = SpiderService.getInstance().getFriendlyRisk(getModel(), getX(), getY(),getSpriteChar() ,getHealth());
-			makeMove(compareRisks(pickupRisk, spartanRisk, friendlyRisk));
-			//quickly compare 3 objects, get the highest object, if his x is bigger than ours, move up, if y is bigger, move left, if both randomize
-			
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-	public void makeMove(DistanceRisk risk)
-	{
-		if(risk.getX()!=this.getX()&&risk.getY()!=this.getY())
-		{
-			int Low = 1;
-			int High = 3;
-			int result = r.nextInt(High-Low) + Low;
-			//if objective is equally far 
-			if(result ==1)
-			{
-				risk.setX(this.getX());
-			}
-			else{
-				risk.setY(this.getY());
-			}
-		}
-		if(risk.getX()>this.getX())
-		{
-			moveDown();
-		}
-		else if(risk.getY()>this.getY())
-		{
-			moveRight();
-		}
-		if(risk.getX()<this.getX())
-		{
-			moveUp();
-		}
-		else if(risk.getY()<this.getY())
-		{
-			moveLeft();
-		}
+		DistanceRisk pickupRisk = SpiderService.getInstance().getPickupRisk(getModel(), getX(), getY(), getHealth());
+		DistanceRisk spartanRisk = SpiderService.getInstance().getSpartanRisk(getX(), getY(), getHealth());
+		DistanceRisk friendlyRisk = SpiderService.getInstance().getFriendlyRisk(getModel(), getX(), getY(),getSpriteChar() ,getHealth());
+		compareRisks(pickupRisk, spartanRisk, friendlyRisk);
 	}
 	public DistanceRisk compareRisks(DistanceRisk ...distanceRisks )
 	{
--- src/ie/gmit/sw/ai/sprites/Spider.java
+++ src/ie/gmit/sw/ai/sprites/Spider.java
@@ -1,80 +1,16 @@
 package ie.gmit.sw.ai.sprites;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import ie.gmit.sw.ai.Maze;
-import ie.gmit.sw.ai.SpriteService;
+import ie.gmit.sw.ai.traversal.Node;
 
 public abstract class Spider extends Moveable{
+	private Node goalNode;
+	private Node lastGoal;
 	//eventually move to classes
-	//FIS fis;
-
-	Random r = new Random();
 	public Spider(Maze model, int x, int y, boolean isAlive, char spriteChar) {
 		super(model, x, y, isAlive, spriteChar);
-		String fileName = "fcl/SpiderMove.fcl";
-        //fis = FIS.load(fileName,true);
 		setHealth(100);
-	}
-	public void eval()
-	{
-		//Variable risk = fis.getFunctionBlock("SpartanSystem").getVariable("risk");
-		//System.out.println("Risk :"+risk.getLatestDefuzzifiedValue());
-	}
-
-	public List<DistanceRisk> getValidMoves()
-	{
-		List<DistanceRisk> dists = new ArrayList<DistanceRisk>();
-		dists.add(new DistanceRisk(this.getX()+1, this.getY()));
-		dists.add(new DistanceRisk(this.getX()-1, this.getY()));
-		dists.add(new DistanceRisk(this.getX(), this.getY()+1));
-		dists.add(new DistanceRisk(this.getX(), this.getY()-1));
-		return dists;
-	}
-
-	public int highestSpartanDist(List<DistanceRisk> a)
-	{
-		Integer highestVal = 0;
-		for (int i = 0; i < a.size(); i++) {
-			if(isValidMove(a.get(i).getX(), a.get(i).getY())){
-				if(spartanDist(a.get(i))>highestVal)
-				{
-					highestVal = i;
-				}
-			}
-			else{
-				a.remove(i);
-			}
-		}
-		if(highestVal==0)
-		{
-			//doesn't matter which direction, so just randomly choose one
-			
-			int Low = 0;
-			int High = a.size();
-			highestVal = r.nextInt(High-Low) + Low;
-		}
-		return highestVal;
-	}
-	public int spartanDist(DistanceRisk p)
-	{
-		SpriteService ss = SpriteService.getInstance();
-		for (int i = 0; i < ss.spritesSize(); i++) {
-			if(ss.getSprite(i) instanceof Spartan)
-			{
-				int xVal = Math.abs(ss.getSprite(i).getX()-p.getX());
-				int yVal = Math.abs(ss.getSprite(i).getY()-p.getY());
-				if(xVal<=10&&yVal<=10)
-				{
-					int averagePercentage = ((xVal+yVal)/2)*10;
-					return averagePercentage;
-				}
-				break;
-			}
-		}
-		return 0;
+		goalNode = lastGoal = null;
 	}
 	@Override
 	public abstract void run();
